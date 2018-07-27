8915b4b97c71a0d593f7727bd330fab366aa5809
Osama Bin Laden <roggstar.pr@googlemail.com>
2018-07-27 12:25:34 +0200

    Ui Finished

--- src/UI.java
+++ src/UI.java
@@ -1,39 +1,60 @@
 import java.awt.Dimension;
 import java.awt.Container;
+import java.awt.Font;
+import java.awt.Color;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-
 import javafx.scene.control.ButtonBar;
 
 public class UI {
 
 	private static JButton addAButton(String text, Container con, JPanel panel,int i) {
-		con.setLayout(new BoxLayout(con, BoxLayout.Y_AXIS));
-        JButton button = new JButton(text);
+        final JButton button = new JButton(text) {
+			{
+				setSize(220, 50);
+				setMaximumSize(getSize());
+			}
+		};
+        button.setBackground(new Color(0x2dce98));
+		button.setForeground(Color.white);
+		button.setFont(new Font("Calibri", Font.PLAIN, 20));
+
 		button.setAlignmentX(con.CENTER_ALIGNMENT);
-		button.setPreferredSize(new Dimension(150, 40));
 		panel.add(button);
+		panel.add(Box.createHorizontalStrut(2));
 
 		ActionListener listener = new ClickListener(i);
 		button.addActionListener(listener);
 		return button;
+	}
+	
+	private static void addALabel(String text, Container con, JPanel panel,int i) {
+		JLabel thumb = new JLabel();
+		thumb.setText(text);
+		thumb.setAlignmentX(con.CENTER_ALIGNMENT);
+		thumb.setFont(new Font("Calibri", Font.PLAIN, i));
+		thumb.setForeground(Color.BLACK);
+		panel.add(thumb);
     }
 
 public static void main(String[] args) {	
-		
+	//initialize pane and frame	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		addAButton("Play (hooman mode)",frame.getContentPane(),panel,0);
-		addAButton("Play (AI mode)",frame.getContentPane(),panel,1);
-		addAButton("Exit game! (fag)",frame.getContentPane(),panel,2);
+	//add objects
+	addALabel("2048", frame.getContentPane(),panel,45);
+	addAButton("Play (hooman mode)",frame.getContentPane(),panel,0);
+	addAButton("Play (AI mode)",frame.getContentPane(),panel,1);
+	addAButton("Exit game! (fag)",frame.getContentPane(),panel,2);
+	addALabel("by Kim Roggenbuck & Phil Roggenbuck", frame.getContentPane(),panel,15);
 		
-		
-		frame.add(panel);
-		
-		frame.setSize(450, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);	
+	//style frame and show it
+	frame.add(panel);
+	frame.setSize(450, 400);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);	
 	
 	}    
 }
